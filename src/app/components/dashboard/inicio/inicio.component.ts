42c82c7ffa1c39ee75fbc989ef4619d4dabd837d
andrelup <andrelcop@gmail.com>
2022-10-29 13:45:26 +0200

    add data to the table

--- src/app/components/dashboard/inicio/inicio.component.ts
+++ src/app/components/dashboard/inicio/inicio.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/interfaces/usuario';
+
+const listUsuarios: Usuario[] = [
+  { usuario: 'jperez', nombre: 'Juan', apellido: 'Perez', sexo: 'Masculino' },
+  { usuario: 'mgomez', nombre: 'Martin', apellido: 'Gomez', sexo: 'Masculino' },
+  {
+    usuario: 'ngarcia',
+    nombre: 'Nicolas',
+    apellido: 'Garcia',
+    sexo: 'Masculino',
+  },
+  { usuario: 'jperez', nombre: 'Juan', apellido: 'Perez', sexo: 'Masculino' },
+  { usuario: 'mgomez', nombre: 'Micaela', apellido: 'Gomez', sexo: 'Femenino' },
+  {
+    usuario: 'ngarcia',
+    nombre: 'Nicolas',
+    apellido: 'Garcia',
+    sexo: 'Masculino',
+  },
+];
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
+  displayedColumns: string[] = [
+    'usuario',
+    'nombre',
+    'apellido',
+    'sexo',
+    'acciones',
+  ];
+  dataSource = listUsuarios;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
